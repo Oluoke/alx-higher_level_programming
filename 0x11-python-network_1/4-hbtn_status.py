94a734f2f82a760dfc5575acf239b51cccb20fe1
Oluoke <princesirgab@yahoo.com>
2024-03-28 14:45:15 -0700

    python-network_1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""Fetch url using requests module
+"""
+ fetches https://alx-intranet.hbtn.io/status
 """
 
 import requests
 
-
 if __name__ == '__main__':
-
-    url = 'https://intranet.hbtn.io/status'
-
-    response = requests.get(url)
-
-    print('Body response:')
-    print('\t- type: {}'.format(type(response.text)))
-    print('\t- content: {}'.format(response.text))
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
